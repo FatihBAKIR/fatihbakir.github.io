0d26cd9779bf5b0bb6a4b3e54f0d19d00d49cc45
Fatih BAKIR <fatih@linux.com>
2019-05-13 22:38:58 -0700

    fix publish script

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ jekyll build
 rm -rf /tmp/fatih/blog
 mkdir -p /tmp/fatih
 pushd /tmp/fatih
-git clone git@github.com:FatihBAKIR/blog
+git clone git@github.com:FatihBAKIR/fatihbakir.github.io blog
 cd blog
 git checkout master
 popd
